0956155a83b96dfffff5c70b3313e3b028078e11
BHARAT SEWANI <bharatsewani1993@gmail.com>
2018-06-28 09:01:14 +0530

    Array Chunk

--- main.go
+++ main.go
@@ -1,33 +1,31 @@
 package main
 
-import(
-  "fmt"
+import (
+	"fmt"
 )
 
-
-func slice_chunk(sliceone []string, size int){
-   chunkslice := make(map[int][]string)
-   var tempchunk []string
-   var count int = 0
-  // var tempchunk []string
-   for i, sliceelement := range sliceone {
-     if count < size {
-       tempchunk = append(tempchunk,sliceelement)
-       count++
-     } else {
-       count = 0
-     }
-     fmt.Printf("Temp Chunk \n",tempchunk)
-     chunkslice[i] = tempchunk
-     tempchunk = tempchunk[:0]
-   }
-   fmt.Printf("Slice Chunk %v\n",chunkslice)
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
 
-func main(){
+func slicechunk(sliceone []string, size int) {
+	chunkslice := make(map[int][]string)
+	var tempchunk []string
+	var count int
+	// var tempchunk []string
+	for i := 0; i < len(sliceone); i += size {
+	  tempchunk = sliceone[i:min(i+size, len(sliceone))]
+    chunkslice[count] = tempchunk
+    count++
+  }
+  fmt.Println("Chunk \n",chunkslice)
+}
+func main() {
 
-     sliceone := []string{"Apple","Mango","Banana","chili","cucumber","Rose"}
-     //slicetwo := []string{"Tomato","Rose","Sunflower","Apple","Mango","Banana"}
-
-     slice_chunk(sliceone,2)
+	sliceone := []string{"Apple", "Mango", "Banana", "chili", "cucumber", "Rose"}
+	//slicetwo := []string{"Tomato","Rose","Sunflower","Apple","Mango","Banana"}
+	slicechunk(sliceone, 2)
 }
